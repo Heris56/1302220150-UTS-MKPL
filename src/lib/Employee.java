f439c48d3b6121fcdae38fded09d1b919032d079
Haikal Risnandar <haikalrisnandar82@gmail.com>
2025-04-18 15:08:47 +0700

    Mengganti primitive Obsession Gender yang menggunakan bolean untuk penentuan laki laki atau perempuan menjadi menggunakan enum yang berisi {laki_laki, Perempuan}

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -19,7 +19,9 @@ public class Employee {
 	private int monthWorkingInYear;
 	
 	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
+        
+        // ganti tipe boolean untuk gender menjadi enum
+	private Gender gender; //true = Laki-laki, false = Perempuan
 	
 	private int monthlySalary;
 	private int otherMonthlyIncome;
@@ -31,7 +33,7 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, Gender gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
---
+++ src/lib/Gender.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package lib;
+
+/**
+ *
+ * @author Haikal
+ */
+public enum Gender {
+    Laki_laki, Perempuan
+}
