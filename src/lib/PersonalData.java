b39b60b4dd9616531cb814477b2cfa80ef3d3ab4
Haikal Risnandar <haikalrisnandar82@gmail.com>
2025-04-18 17:05:05 +0700

    memperbaiki Data Clumps data personal Employee
    melakukan refactoring dengan extract class data data employee menjadi
    PersonalData yang isinya firstname, lastname, idnumber, address

---
+++ src/lib/PersonalData.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lib;
+
+/**
+ *
+ * @author Haikal
+ */
+public class PersonalData {
+    private String firstName;
+    private String lastName;
+    private String idNumber;
+    private String address;
+    
+    PersonalData(String firstName, String lastName, String idNumber, String address){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+        this.address = address;
+    }
+}
